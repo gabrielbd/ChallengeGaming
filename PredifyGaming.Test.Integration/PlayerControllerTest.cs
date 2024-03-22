99a377bf0cc02f87a96f64512d61c647d1724e38
Gabriel <gabrieldbronisio@gmail.com>
2024-03-22 13:26:31 -0300

    ✍🏼 Naming correction

--- PredifyGaming.Test.Integration/PlayerControllerTest.cs
+++ PredifyGaming.Test.Integration/PlayerControllerTest.cs
@@ -2,32 +2,31 @@
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Newtonsoft.Json;
-using PredifyGaming.Application.Commands.PlaysResult;
+using PredifyGaming.Application.Commands.Players;
 using System.Net;
 using System.Text;
 using Xunit;
 
 namespace PredifyGaming.Test.Integration
 {
-    public class PlaysControlTest
+    public class PlayerControllerTest
     {
 
         [Fact]
-        public async Task TestCreatedPlays()
+        public async Task TestCreatedPlayer()
         {
             var faker = new Faker("pt_BR");
 
-            var dto = new PlaysResultDTO
+            var dto = new PlayersDTO
             {
-                PlayerId = 1,
-                GameId = 1,
+                Name = faker.Person.FullName,
             };
 
             var content = new StringContent(JsonConvert.SerializeObject(dto),
                 Encoding.UTF8, "application/json");
 
             var result = await new WebApplicationFactory<Program>()
-                     .CreateClient().PostAsync("/api/Plays", content);
+                     .CreateClient().PostAsync("/api/Player", content);
 
             result.StatusCode
                 .Should()
--- PredifyGaming.Test.Integration/PlaysControllerTest.cs
+++ PredifyGaming.Test.Integration/PlaysControllerTest.cs
@@ -2,31 +2,32 @@
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Newtonsoft.Json;
-using PredifyGaming.Application.Commands.Players;
+using PredifyGaming.Application.Commands.PlaysResult;
 using System.Net;
 using System.Text;
 using Xunit;
 
 namespace PredifyGaming.Test.Integration
 {
-    public class PlayerControlTest
+    public class PlaysControllerTest
     {
 
         [Fact]
-        public async Task TestCreatedPlayer()
+        public async Task TestCreatedPlays()
         {
             var faker = new Faker("pt_BR");
 
-            var dto = new PlayersDTO
+            var dto = new PlaysResultDTO
             {
-                Name = faker.Person.FullName,
+                PlayerId = 1,
+                GameId = 1,
             };
 
             var content = new StringContent(JsonConvert.SerializeObject(dto),
                 Encoding.UTF8, "application/json");
 
             var result = await new WebApplicationFactory<Program>()
-                     .CreateClient().PostAsync("/api/Player", content);
+                     .CreateClient().PostAsync("/api/Plays", content);
 
             result.StatusCode
                 .Should()
